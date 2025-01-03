d09253c398c660ca262a055d8d91531cf8b1c222
Vk497030 <vk497030@gmail.com>
2025-01-03 14:22:14 +0530

    Footer changes

--- src/components/portfolio/Footer.jsx
+++ src/components/portfolio/Footer.jsx
@@ -7,17 +7,17 @@ const Footer = () => {
     <footer className="bg-white py-8 px-4 mt-10">
       <div className="container mx-auto flex flex-col  items-center">
         <div className="flex space-x-6 mb-6">
-          <Link to="https://www.linkedin.com/in/sahityakashyap/" target='_blank' className="text-black hover:text-gray-600">
+          <Link to="https://www.linkedin.com/in/vikaskrdesigns/" target='_blank' className="text-black hover:text-gray-600">
             <LinkedinIcon size={24} />
             <span className="sr-only">LinkedIn</span>
           </Link>
 
-          <Link to="https://www.behance.net/gallery/206534693/Industrial-Design-Portfolio-2024" target='_blank' className="text-black hover:text-gray-600">
+          <Link to="https://www.behance.net/vikaskr" target='_blank' className="text-black hover:text-gray-600">
           <i className="fa-brands text-[25px] mt-[1px] mt fa-behance"></i>
           
             <span className="sr-only">Behance</span>
           </Link>
-          <Link to="https://www.instagram.com/sahityakashyap.in/" target='_blank' className="text-black hover:text-gray-600">
+          <Link to="https://www.instagram.com/vikas.k.r/" target='_blank' className="text-black hover:text-gray-600">
             <Instagram size={24} />
             <span className="sr-only">Instagram</span>
           </Link>
@@ -26,7 +26,7 @@ const Footer = () => {
           DOWNLOAD RESUME
         </Link>
         <p className="text-xs text-gray-500">
-          © Sahitya Kashyap 2024 All Rights Reserved
+          © Vikas Kumar 2024 All Rights Reserved
         </p>
         <p className='text-xs text-gray-500 mt-1 '>Website Developed by <Link to="https://www.linkedin.com/in/daya-sagar-903676179?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bepu1n%2F1zTYqXbC%2BSnGKq6A%3D%3D" target='_blank' className='hover:underline text-gray-700'>Daya Sagar</Link> </p>
       </div>
